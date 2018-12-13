801ec3c281125dba95b64058de6f3bc9f55b6d7d
MrMokka <sartaren@hotmail.com>
2018-12-13 17:01:07 +0100

    GEO Map update, not finished

--- Assets/Scripts/GPSScripts/GPS.cs
+++ Assets/Scripts/GPSScripts/GPS.cs
@@ -8,8 +8,8 @@ public class GPS : MonoBehaviour {
 
     public Text text;
 
-    private float latitude;
-    private float longitude;
+    private float latitude = 59.912666f;
+    private float longitude = 10.746467f;
 
     private bool debug = false;
 
@@ -17,45 +17,47 @@ public class GPS : MonoBehaviour {
 	private void Start(){
 	    //Instance = this;
         //DontDestroyOnLoad(gameObject);
-	    StartCoroutine(StartLocationService());
+	    //StartCoroutine(StartLocationService());
+	    TranslateCoord();
+            
+            
+	    transform.position = new Vector3(longitude, 0, latitude);
 	}
 
     private IEnumerator StartLocationService() {
-        
-        if (!Input.location.isEnabledByUser)
-        {
-            Debug.Log("User has not enabled GPS");
-            yield break;
-        }
 
-        Input.location.Start();
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-        {
-            yield return new WaitForSeconds(1);
-            maxWait--;
+        while(true){
+            if (!Input.location.isEnabledByUser){
+                Debug.Log("User has not enabled GPS");
+                yield break;
+            }
+    
+            Input.location.Start();
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0){
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
+    
+            if (maxWait <= 0){
+                Debug.Log("Timed out");
+                yield break;
+            }
+    
+            if (Input.location.status == LocationServiceStatus.Failed){
+                Debug.Log("Unable to determin device location");
+                yield break;
+            }
+    
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+            
+            
+            TranslateCoord();
+            
+            
+            transform.position = new Vector3(longitude, 0, latitude);
         }
-
-        if (maxWait <= 0)
-        {
-            Debug.Log("Timed out");
-            yield break;
-        }
-
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            Debug.Log("Unable to determin device location");
-            yield break;
-        }
-
-        latitude = Input.location.lastData.latitude;
-        longitude = Input.location.lastData.longitude;
-        
-        
-        TranslateCoord();
-        
-        
-        transform.position = new Vector3(longitude, 0, latitude);
         
     }
 
@@ -65,26 +67,28 @@ public class GPS : MonoBehaviour {
     private void TranslateCoord(){
         
         
-        string sLat = latitude.ToString().Substring(4);
-        sLat = sLat.Insert(1, ".");
-        latitude = float.Parse(sLat);
-
-        if(debug){
-
-            float fLng = (76 - longitude) * 75;
-            longitude = fLng;
-
-        } else{
-            string sLng = longitude.ToString().Substring(4);
-
-            sLng = sLng.Insert(1, ".");
-
-            longitude = float.Parse(sLng);
-        }
         
 
-        text.text = latitude + " : " + longitude;
+        if(!debug){
+            
+            string sLat = latitude.ToString().Substring(4);
+            sLat = sLat.Insert(1, ".");
+            latitude = float.Parse(sLat);
+        
+            string sLng = longitude.ToString().Substring(4);
+            sLng = sLng.Insert(1, ".");
+            longitude = float.Parse(sLng);
 
+            float fLng = longitude * 12.97f;
+            float fLat = latitude * 12.97f;
+            longitude = fLng;
+            latitude = fLat;
+
+        }
+        
+        text.text = latitude + " : " + longitude;
+        
+        
     }
 
 
