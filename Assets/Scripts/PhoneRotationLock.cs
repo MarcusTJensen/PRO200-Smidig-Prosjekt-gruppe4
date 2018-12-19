27b02724e8bbf5ab2faa7569be8f86b4f959a38f
MrMokka <sartaren@hotmail.com>
2018-12-19 14:08:56 +0100

    Removed placeholders, added phonerotation, moved pictures

--- Assets/Scripts/PhoneRotationLock.cs
+++ Assets/Scripts/PhoneRotationLock.cs
@@ -1,8 +1,32 @@
 ﻿using UnityEngine;
 
 public class PhoneRotationLock : MonoBehaviour{
-	private void Start(){
+	
+	
+	
+	
+	private void Awake(){
+		DontDestroyOnLoad(gameObject);
+		DisableLandscape();
+	}
+
+
+
+
+
+	public void DisableLandscape(){
 		Screen.autorotateToLandscapeLeft = false;
 		Screen.autorotateToLandscapeRight = false;
+		Screen.autorotateToPortrait = true;
+		Screen.autorotateToPortraitUpsideDown = true;
 	}
+	
+	public void EnableLandscape(){
+		Screen.autorotateToLandscapeLeft = true;
+		Screen.autorotateToLandscapeRight = true;
+		Screen.autorotateToPortrait = false;
+		Screen.autorotateToPortraitUpsideDown = false;
+	}
+	
+	
 }
